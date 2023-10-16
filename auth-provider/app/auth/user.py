d78f6779bb1625f1c51307c16e417b4e30aa78bc
amiran <amiran.kalandia@highpots.com>
2023-10-16 12:25:19 +0400

    Created auth provider project

---
+++ auth-provider/app/auth/user.py
@@ -0,0 +1,56 @@
+from typing_extensions import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+from jwt.exceptions import PyJWTError
+from app.auth.token import ALGORITHM, SECRET_KEY
+from app.schemas.token import TokenData
+from app.schemas.user import UserInDB, User
+from app.models.users import get_fake_user_from_db
+from app.auth.password import verify_password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except PyJWTError:
+        raise credentials_exception
+    user = get_user(get_fake_user_from_db(), username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
---
+++ auth-provider/app/models/users.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+
+def get_fake_user_from_db() -> Dict:
+    fake_users_db = {
+        "johndoe": {
+            "username": "johndoe",
+            "full_name": "John Doe",
+            "email": "johndoe@example.com",
+            "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+            "disabled": False,
+        }
+    }
+    return fake_users_db
